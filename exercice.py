4e00f209e7e7abf9e7412aec6c1e04b585f4499e
ThomasFontier <thomas.fontier@polymtl.ca>
2020-09-14 23:29:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,7 +24,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 def to_degrees(angle_rads: float) -> tuple:
     z = math.degrees(angle_rads)
     deg = int(z)
-    z_2 = z-deg
+    z_2 = (z-deg)*60
     min = 
     return 
 

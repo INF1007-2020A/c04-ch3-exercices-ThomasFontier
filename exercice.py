85d2a71497dea57df2ec730c5ec6a83ee42c9065
ThomasFontier <thomas.fontier@polymtl.ca>
2020-09-14 23:26:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,7 +23,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     z = math.degrees(angle_rads)
-    deg = mag
+    deg = math.
     return 
 
 

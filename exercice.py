54cce3293b29ae837795138d9132674b7941b1ae
ThomasFontier <thomas.fontier@polymtl.ca>
2020-09-14 23:24:37 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def average(a: float, b: float, c: float) -> float:
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     z = angle_degs + angle_mins/60 + angle_secs/3600
-    return math.ra
+    return math.radians(z)
 
 
 def to_degrees(angle_rads: float) -> tuple:
